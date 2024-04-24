0ddb28f36ffe7ae5dfebc6a0c63b525ced3d218a
Andrejus <andrejus.iv1880@gmail.com>
2024-04-24 20:49:12 +0300

    Adding class with full structure of Json and some HHTP implementations

---
+++ FlightDataWebScraper/FlightDataWebScraper/Json.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightDataWebScraper
+{
+    public class Json
+    {
+
+        public class Airport
+        {
+            public string Code { get; set; }
+            public string Description { get; set; }
+            public bool Resident { get; set; }
+            public string Type { get; set; }
+            public string Zone { get; set; }
+            public string Image { get; set; }
+        }
+
+        public class Flote
+        {
+            public string Code { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class TechnicalStop
+        {
+            public int NumberStops { get; set; }
+            public List<string> AirportStops { get; set; }
+        }
+
+        public class CabinClass
+        {
+            public string Code { get; set; }
+            public string Description { get; set; }
+        }
+
+        public class Flight
+        {
+            public string Number { get; set; }
+            public Airport AirportDeparture { get; set; }
+            public Airport AirportArrival { get; set; }
+            public string DateDeparture { get; set; }
+            public string DateArrival { get; set; }
+            public string GmtDateDeparture { get; set; }
+            public string GmtDateArrival { get; set; }
+            public string CompanyCode { get; set; }
+            public string Operator { get; set; }
+            public Flote Flote { get; set; }
+            public TechnicalStop TechnicalStop { get; set; }
+            public string TerminalDeparture { get; set; }
+            public string TerminalArrival { get; set; }
+            public CabinClass CabinClass { get; set; }
+        }
+
+        public class Journey
+        {
+            public int RecommendationId { get; set; }
+            public int Identity { get; set; }
+            public string Direction { get; set; }
+            public CabinClass CabinClass { get; set; }
+            public decimal ImportChild { get; set; }
+            public decimal ImportInfant { get; set; }
+            public decimal ImportAdultResident { get; set; }
+            public decimal ImportChildResident { get; set; }
+            public decimal ImportInfantResident { get; set; }
+            public decimal DiscountAdultResident { get; set; }
+            public decimal DiscountChildResident { get; set; }
+            public decimal DiscountInfantResident { get; set; }
+            public decimal ImportTaxAdl { get; set; }
+            public decimal ImportTaxChd { get; set; }
+            public decimal ImportTaxInf { get; set; }
+            public string ClassCode { get; set; }
+            public string FarebasisCode { get; set; }
+            public string PromotionLabel { get; set; }
+            public List<Flight> Flights { get; set; }
+        }
+
+        public class Availability
+        {
+            public int RecommendationId { get; set; }
+            public double Total { get; set; }
+        }
+
+        public class Data
+        {
+            public string SessionId { get; set; }
+            public string AvailabilityId { get; set; }
+            public string Locale { get; set; }
+            public string MarketCode { get; set; }
+            public bool Swirt { get; set; }
+            public bool Switax { get; set; }
+            public bool Swisdto { get; set; }
+            public int AdultPax { get; set; }
+            public int ChildPax { get; set; }
+            public int InfantPax { get; set; }
+            public int AdultPaxResident { get; set; }
+            public int ChildPaxResident { get; set; }
+            public int InfantPaxResident { get; set; }
+            public List<object> MessageItemization { get; set; }  // List<object> if itemization structure is unknown
+            public decimal ServiceFee { get; set; }
+            public decimal ServiceFeeDiscount { get; set; }
+            public decimal ServiceFeeResidentDiscount { get; set; }
+            public List<Availability> TotalAvailabilities { get; set; }
+            public List<Journey> Journeys { get; set; }
+        }
+
+        public class Body
+        {
+            public Data Data { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Body Body { get; set; }
+        }
+    }
+}
--- FlightDataWebScraper/FlightDataWebScraper/Program.cs
+++ FlightDataWebScraper/FlightDataWebScraper/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var url = "http://homeworktask.infare.lt/search.php?from=MAD&to=FUE&depart=2024-05-09&return=2024-05-16";
+
+        using HttpClient client = new HttpClient();
+
+        var scraper = new WebScraperHttp();
+        string jsonData = await scraper.FetchDataAsync(url);
+
+        try
+        {
+
+            FlightDataWebScraper.Json.RootObject rootObject = JsonConvert.DeserializeObject<FlightDataWebScraper.Json.RootObject>(jsonData);
+
+            if (rootObject?.Body?.Data?.Journeys != null)
+            {
+                foreach (var journey in rootObject.Body.Data.Journeys)
+                {
+                    foreach (var flight in journey.Flights)
+                    {
+                        Console.WriteLine($"Flight Number: {flight.Number}");
+                    }
+                }
+
+            }
+                //var desirializeObject = JsonConvert.DeserializeObject(jsonData); //var arba dynamic
+                //Console.WriteLine(desirializeObject);
+                //Console.ReadLine();
+
+                //var flights = desirializeObject.body.data.journeys.flights;
+
+                //var outboundFlights = flights.body.data.journeys.FindAll(f => f.DepartureAirport == "MAD" && f.ArrivalAirport == "AUH");
+
+                //List<FlightInformation> data = JsonConvert.DeserializeObject<List<FlightInformation>>(jsonData);
+
+
+
+                // Extracting all the flights
+                // var flights = desirializeObject.body.data.journeys.flights;
+
+                //Filter all the flights from MAD - AUH
+                //var madToAuhFlights = flights.Where(f1 => f1.airportDeparture.code == "MAD" && f1.airportArrivals.code == "AUH");
+
+                // Extract all the prices for each recomentadion
+
+                //var prices = desirializeObject.body.totalAvailabilities.ToDictionary(p => (int)p.recommendationId, p => (double)p.total);
+
+
+
+                //Console.WriteLine("");
+                //Console.ReadKey();
+
+                //List<FlightData> data = JsonConvert.DeserializeObject<List<FlightData>>(json);
+
+                //WriteToCsv(data, @"C:\Users\andre\Desktop\FlightData.csv");
+
+                Console.WriteLine("Data scraped and saved to flights.csv successfully.");
+                Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        // todo : create object with properties that need to be filled in the the csv...
+    }
+}
