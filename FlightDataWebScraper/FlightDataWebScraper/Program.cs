05192310f3e194b6653c335bba2177b3cb4ba851
Andrejus Ivascenko <andrejus.iv1880@gmail.com>
2024-04-25 09:02:32 +0300

    Sekmingas pataisymas Json skaityme

--- FlightDataWebScraper/FlightDataWebScraper/Program.cs
+++ FlightDataWebScraper/FlightDataWebScraper/Program.cs
@@ -15,51 +15,54 @@ class Program
         try
         {
 
-            FlightDataWebScraper.Json.RootObject rootObject = JsonConvert.DeserializeObject<FlightDataWebScraper.Json.RootObject>(jsonData);
+            //FlightDataWebScraper.Json.RootObject rootObject = JsonConvert.DeserializeObject<FlightDataWebScraper.Json.RootObject>(jsonData);
 
-            if (rootObject?.Body?.Data?.Journeys != null)
-            {
-                foreach (var journey in rootObject.Body.Data.Journeys)
-                {
-                    foreach (var flight in journey.Flights)
-                    {
-                        Console.WriteLine($"Flight Number: {flight.Number}");
-                    }
-                }
+            //if (rootObject?.Body?.Data?.Journeys != null)
+            //{
+            //    foreach (var journey in rootObject.Body.Data.Journeys)
+            //    {
+            //        foreach (var flight in journey.Flights)
+          //          {
+           //             Console.WriteLine($"Flight Number: {flight.Number}");
+          //          }
+          //      }
 
-            }
-                //var desirializeObject = JsonConvert.DeserializeObject(jsonData); //var arba dynamic
+          //  }
+                dynamic desirializeObject = JsonConvert.DeserializeObject(jsonData); //var arba dynamic
                 //Console.WriteLine(desirializeObject);
                 //Console.ReadLine();
 
-                //var flights = desirializeObject.body.data.journeys.flights;
+                var flights = desirializeObject.body.data.journeys[1].flights; // <---- VEIKIA!!!!!!!!
 
-                //var outboundFlights = flights.body.data.journeys.FindAll(f => f.DepartureAirport == "MAD" && f.ArrivalAirport == "AUH");
+            Console.WriteLine(flights);
+            Console.ReadLine();
 
-                //List<FlightInformation> data = JsonConvert.DeserializeObject<List<FlightInformation>>(jsonData);
+            //var outboundFlights = flights.body.data.journeys.FindAll(f => f.DepartureAirport == "MAD" && f.ArrivalAirport == "AUH");
 
-
-
-                // Extracting all the flights
-                // var flights = desirializeObject.body.data.journeys.flights;
-
-                //Filter all the flights from MAD - AUH
-                //var madToAuhFlights = flights.Where(f1 => f1.airportDeparture.code == "MAD" && f1.airportArrivals.code == "AUH");
-
-                // Extract all the prices for each recomentadion
-
-                //var prices = desirializeObject.body.totalAvailabilities.ToDictionary(p => (int)p.recommendationId, p => (double)p.total);
+            //List<FlightInformation> data = JsonConvert.DeserializeObject<List<FlightInformation>>(jsonData);
 
 
 
-                //Console.WriteLine("");
-                //Console.ReadKey();
+            // Extracting all the flights
+            // var flights = desirializeObject.body.data.journeys.flights;
 
-                //List<FlightData> data = JsonConvert.DeserializeObject<List<FlightData>>(json);
+            //Filter all the flights from MAD - AUH
+            //var madToAuhFlights = flights.Where(f1 => f1.airportDeparture.code == "MAD" && f1.airportArrivals.code == "AUH");
 
-                //WriteToCsv(data, @"C:\Users\andre\Desktop\FlightData.csv");
+            // Extract all the prices for each recomentadion
 
-                Console.WriteLine("Data scraped and saved to flights.csv successfully.");
+            //var prices = desirializeObject.body.totalAvailabilities.ToDictionary(p => (int)p.recommendationId, p => (double)p.total);
+
+
+
+            //Console.WriteLine("");
+            //Console.ReadKey();
+
+            //List<FlightData> data = JsonConvert.DeserializeObject<List<FlightData>>(json);
+
+            //WriteToCsv(data, @"C:\Users\andre\Desktop\FlightData.csv");
+
+            Console.WriteLine("Data scraped and saved to flights.csv successfully.");
                 Console.ReadLine();
         }
         catch (Exception ex)
